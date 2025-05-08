46cd1e999095db845e916987fae69128527b659a
Senn <122393914+WassyWasbeer@users.noreply.github.com>
2025-05-08 23:27:46 +0200

    Update random number.py

--- random number.py
+++ random number.py
@@ -8,7 +8,7 @@ weapon_dict = {0:"auto rifle", 1:"dual shotgun", 2:"dual pistols", 3:"auto shotg
                8:"laser cannon", 9:"crossbow", 10:"orb launcher", 11:"rocket launcher", 12:"minigun", 13:"blade launcher",
                14:"cluster launcher", 15:"marksman rifle", 16:"arcane wand", 17:"ice staff", 18:"lightning scepter"}
 
-nade_dict = {0:"gremade", 1:"grappling hook", 2:"black hole", 3:"laser beam", 4:"ice blast", 5:"electro globe", 6:"air strike"}
+nade_dict = {0:"grenade", 1:"grappling hook", 2:"black hole", 3:"laser beam", 4:"ice blast", 5:"electro globe", 6:"air strike"}
 
 melee_dict = {0:"claw", 1:"dagger", 2:"hammer", 3:"pickaxe", 4:"katana"}
 
